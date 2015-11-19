7c966fabe620fe93eccb97669ac15c4c18350ce0
Ridvan <Ridvan@Ridvan>
2015-11-19 09:57:57 +0100

    Alle Dateien von Ridvan Atacan Master of Desaster

---
+++ app/models/LieferAdresse_Model.php
@@ -0,0 +1,54 @@
+<!-- Ridvan Atacan, 3113837
+    10.11.2015 Group #4 Onlineshop
+    Sprint 2, Task : 140-6 #10204
+    User Story: Als Programmierer möchte ich meine Codes als Model–view–controller (MVC) haben
+    Task: Eigenen Code an MVC anpassen
+    Aufwand: 5 Stunden
+ -->
+<?php
+
+//Zugriff auf die Datenbankverbindungsklasse
+include "../config/Connect_Mysql.php";
+
+class LieferAdresse_Model{
+   
+   private $sql; 
+   private $name;
+   private $nname;
+   private $strasse;
+   private $plz;
+   private $kid;
+    //Datenbankverbindung versuchen aufzubauen sobald ein Objekt der Klasse erzeugt wird
+    public function __construct(){
+        
+         try{
+           $this->db = new Connect_Mysql();
+           $this->con = $this->db->verbinden();
+            
+            
+        }catch(PDOException $ex){
+            
+            print $ex;
+    }
+        
+    }
+    //Methode zum Hinzufügen einer Lieferadresse anhand der $_POST- Parameter die vom View übergeben werden
+    public function hinzufügen($name,$nname,$strasse,$plz,$kid){
+        
+        
+        $this->sql ='INSERT INTO Lieferadresse(Vorname,Nachname,Straße,Postleitzahl_PLZ,Kunde_Kundennummer) '
+		. 'VALUES 	("'. $name .'","' . $nname . '","' . $strasse . '","' . $plz. '","'. $kid .'");';
+        $this->con->query($this->sql);
+        $this->con = null;
+        $this->closeDB();
+         
+        echo "Die neue Lieferadresse wurde angelegt!";
+    }
+    // Methode um die Datenbankverbindung zu trennen
+    public function closeDB(){
+        $this->db->schließen();
+        $this->con = 0;
+    }
+    
+}
+
---
+++ app/models/MeineBestellungen_Model.php
@@ -0,0 +1,125 @@
+<!-- Ridvan Atacan, 3113837
+    10.11.2015 Group #4 Onlineshop
+    Sprint 2, Task : 170-1 #10197
+    User Story: Als Kunde möchte ich meine alten Bestellungen darstellen können. 
+    Task: Alte Bestellungen anzeigen
+    Aufwand: 10 Stunden
+ -->
+<?php
+// Zugriff auf die Datenbankverbindungsklasse
+include "../config/Connect_Mysql.php";
+//include "../config/Login.php";
+
+class MeineBestellungen_Model{
+		
+    private $con;
+    private $bestellnummer;
+    private $datum;
+    private $gesamtpreis;
+    private $menge;
+    private $data;
+    private $db;
+    private $sql;
+    private $result;
+    
+    //Datenbankverbindung versuchen aufzubauen sobald ein Objekt der Klasse erzeugt wird
+    public function __construct(){
+       
+        try{
+           $this->db = new Connect_Mysql();
+           $this->con = $this->db->verbinden();
+            
+            
+        }catch(PDOException $ex){
+            
+            print $ex;
+    }
+    }
+	//Methode zur Ausgabe von der Bestellnummer zum zugehörigen Kunden
+     public function bestellliste() {
+        
+		//Zugriff auf die Tabellen auf der Datenbank mittels SQL Statements und PDO
+        echo '<br>Meine Bestellungen <br>';
+        $this ->sql = $this->con->prepare("SELECT Bestellnummer FROM BESTELLUNG WHERE Kunde_Kundennummer = 6");
+        $this ->sql->execute();
+        $this->data = $this->sql->fetch();
+        $this->bestellnummer = $this->data[0];
+        
+        $cnt = $this->sql->rowCount();
+        echo("Bestellnummer : <br>");
+        echo($this->bestellnummer);
+        
+         $this->closeDB();
+        
+        }
+	//Methode zur Ausgabe aller Bestellungen zum zugehörigen Kunden
+	//Im weiteren Verlauf wird es möglich sein nur dem eingeloggten Kunden seine Bestellungen darzustellen
+     public function alleBestellungen(){
+        
+		//Zugriff auf die Tabellen auf der Datenbank mittels SQL Statements und PDO		
+        echo'<br><strong>Alle Bestellungen</br></strong>';
+        $query = "SELECT Bestellnummer,Gesamtpreis,Menge,Datum FROM Bestellung WHERE Kunde_Kundennummer = 5" ;
+        $this->sql = $this->con->prepare($query);
+        $this->sql->execute();
+        $this->result = $this->sql->fetch(PDO::FETCH_ASSOC);
+        $this->count = $this->sql->rowCount();
+        
+		//Abfrage, ob dem jeweiligen Kunden eine Bestellung vorliegt
+         if(empty($this->result)){
+            
+            echo"Sie haben noch keine Bestellungen:<br> ";
+             
+        }else{
+           
+            echo"Sie haben folgende Bestellungen:<br> "
+            . "Bestellungen : " .$this->count ."<br>";
+        }
+        
+        $i = 0;
+		// Wenn mehrere Zeilen von der Datenbank gelesen werden, diese durchgehen und Zeile für Zeile ausgeben
+        while ($i < $this->count){
+            echo"<br><fieldset>";
+            echo "Bestellnummer : " .$this->result['Bestellnummer'];
+            echo "<br>Gesamtpreis : " .$this->result['Gesamtpreis'];
+            echo "<br>Menge : " .$this->result['Menge'];
+            echo "<br>Datum : " .$this->result['Datum'];
+            echo "</fieldset>";
+            
+           
+           
+            $i++;
+            $this->result = $this->sql->fetch(PDO::FETCH_ASSOC);
+            
+        }
+        $this->gesamtpreis = $this->result['Gesamtpreis'];
+        
+        echo "</table>";
+        
+        
+        $this->closeDB();
+     }        
+        
+    public function prnt(){
+        print $this->bestellnummer;
+    }
+	//Methode um die Datenbankverbindung zu trennen
+    public function closeDB(){
+       $this->db->schließen();
+       $this->con = 0;
+   }
+   
+   public function getPreis(){
+       
+       return $this->gesamtpreis;
+   }
+   
+   public function getRow(){
+        
+      return $this->count;
+   }
+        
+}
+/*obj = new MeineBestellungen_Model();
+$obj->prnt();
+$obj->bestellliste();
+*/

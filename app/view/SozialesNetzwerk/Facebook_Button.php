1a9baa71d5ce0c3995607614524eefdbf0eafec4
Ridvan <Ridvan@Ridvan>
2015-11-19 10:51:33 +0100

    Alle Dateien von Ridvan Atacan Master of Desaster

---
+++ app/view/SozialesNetzwerk/Facebook_Button.php
@@ -0,0 +1,23 @@
+<!-- Ridvan Atacan, 3113837
+    17.11.2015 Group #4 Onlineshop
+    Sprint 3, Task : 240-1 #10324
+    User Story: Als Kunde möchte ich den OnlineShop auf verschieden Sozialen Netzwerken liken können
+    Task: Verlinkung mit Sozialen Netzwerken erstellen 
+    Aufwand: --
+ -->
+<html lang="de">
+
+    <body>
+        <!-- Von Facebook generierter Code, der dem Kunden die Möglichkeit bietet, unsere Facebook-Seite durch einen eingebundenen Button zu liken -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v2.5";
+          fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
+        <!-- Aufruf des Plugins an gewünschter Stelle -->
+        <div class="fb-like" data-href="https://www.facebook.com/Fashion-Factory-947926495277944/?ref=hl" data-width="450" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+    </body>
+
---
+++ app/view/SozialesNetzwerk/Google_Plus.php
@@ -0,0 +1,16 @@
+<!-- Ridvan Atacan, 3113837
+    17.11.2015 Group #4 Onlineshop
+    Sprint 3, Task : 240-1 #10324
+    User Story: Als Kunde möchte ich den OnlineShop auf verschieden Sozialen Netzwerken liken können
+    Task: Verlinkung mit Sozialen Netzwerken erstellen 
+    Aufwand: --
+ -->
+
+
+<!-- Von Google + generierter Code, der dem Kunden die Möglichkeit bietet, unsere Google Plus-Seite durch einen eingebundenen Button zu empfehlen -->
+<script src="https://apis.google.com/js/platform.js" async defer>
+  {lang: 'de'}
+</script>
+
+<!-- Aufruf des Plugins an gewünschter Stelle -->
+<div class="g-plusone" data-href="https://plus.google.com/u/0/b/100266200116052598197/100266200116052598197/about"></div>

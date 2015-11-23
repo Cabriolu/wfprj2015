f07fc270edb0b713162e4d95fa3276091b9643dc
#Chris <#Chris@141.59.82.46>
2015-11-23 18:55:08 +0100

    mein warenkorb chris

---
+++ app/view/Login/LoginInfo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <title></title>
+        
+    </head>
+    <body>
+       
+        <h1>Herzlich Willkommen User</h1>
+        
+    </body>
+</html>
---
+++ app/view/Login/LoginView.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <title></title>
+        
+    </head>
+    <body>
+       
+        <form action="LoginController/login" method="post">
+            <label>Email</label><input type="text" name="mail" size="24" maxlength="50"/><br/>
+            <label>Passwort</label><input type="password" name="pw" size="24" maxlength="50"/><br/>
+            <label></label><input type="submit" name="submit" value="Bestätigen"/>
+        </form>
+        
+    </body>
+</html>

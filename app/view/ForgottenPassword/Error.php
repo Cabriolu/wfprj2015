9e41a6f8bbc381382915017717adf2e384c100f6
Yerlikaya <Yerlikaya@141.59.82.99>
2015-12-01 12:14:09 +0100

    Hanim: View gändert 01.12.2015

---
+++ app/view/ForgottenPassword/Error.php
@@ -0,0 +1,17 @@
+<!-- Header ändern! -->
+<!-- --Sprint 3, Gruppe 4 Onlineshop, 
+Verfasser: Hanim Yerlikaya, Datum: 24.11.2015
+UserStory: Passwortzurücksetzung mit automatischen Email-Versand programmieren
+Task: 200-1 (#10321) Passwort zurück setzen
+Aufwand: 5 Stunden
+Beschreibung: Der User kann ein einen neuen Passwort beantragen.   
+
+Mysql-Connection benutzt von Kerstin Gräter  -->
+
+
+<!-- Wenn die Email-Adresse nicht vorhanden ist, gibt es die Meldung-->
+<main>
+    <h3>Error</h3>
+    <p>An error occured and could not send an email to <?php print $data; ?>.</p>
+</main>
+
